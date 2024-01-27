208034a40d1c639f83cd5c64faf8ea7bb20032cc
Jermiah Joseph <jermiahjoseph98@gmail.com>
2024-01-27 14:40:08 -0500

    chore(NBIA_ENDPOINTS): add some endpoints and general documentation

--- src/nbiatoolkit/utils/nbia_endpoints.py
+++ src/nbiatoolkit/utils/nbia_endpoints.py
@@ -6,28 +6,35 @@ class NBIA_ENDPOINTS(Enum):
     """
     BASE_URL = 'https://services.cancerimagingarchive.net/nbia-api/services/'
     
-    GET_COLLECTION_PATIENT_COUNT = 'getCollectionValuesAndCounts'
     GET_COLLECTIONS = 'v2/getCollectionValues'
+    GET_COLLECTION_PATIENT_COUNT = 'getCollectionValuesAndCounts'
+    GET_COLLECTION_DESCRIPTIONS = "getCollectionDescriptions"
+
+    GET_PATIENT_BY_COLLECTION_AND_MODALITY = 'v2/getPatientByCollectionAndModality'
     GET_BODY_PART_PATIENT_COUNT = 'getBodyPartValuesAndCounts'
-    GET_PATIENT_BY_COLLECTION_AND_MODALITY = 'v2/getPatientByCollectionAndModality'    
+    GET_NEW_PATIENTS_IN_COLLECTION = "NewPatientsInCollection"
+
+
+
     GET_SERIES = 'v2/getSeries'
-    
     DOWNLOAD_SERIES = 'v2/getImageWithMD5Hash'
+
     
     
-    GET_SERIES_SIZE = 'v2/getSeriesSize'        # TIMES OUT???
-    GET_UPDATED_SERIES = 'v2/getUpdatedSeries'
+    GET_UPDATED_SERIES = 'v2/getUpdatedSeries' # ?fromDate=01/01/2024
         
+
     # https://services.cancerimagingarchive.net/nbia-api/services/v2/getSeriesMetaData?SeriesInstanceUID=1.3.6.1.4.1.9590.100.1.2.374115997511889073021386151921807063992
     
     # https://services.cancerimagingarchive.net/nbia-api/services/v2/getSeriesSize?SeriesInstanceUID=1.3.6.1.4.1.9590.100.1.2.374115997511889073021386151921807063992
-    
+
     # curl -H "Authorization:Bearer YOUR_ACCESS_TOKEN" -k "https://services.cancerimagingarchive.net/nbia-api/services/v2/getSeries"
     # curl -H "Authorization:Bearer YOUR_ACCESS_TOKEN" -k "https://services.cancerimagingarchive.net/nbia-api/services/v2/getSeriesMetaData"
     # curl -H "Authorization:Bearer YOUR_ACCESS_TOKEN" -k "https://services.cancerimagingarchive.net/nbia-api/services/v2/getSeriesSize"
-    
+
     # curl -H "Authorization:Bearer YOUR_ACCESS_TOKEN" -k "https://services.cancerimagingarchive.net/nbia-api/services/v2/getUpdatedSeries"
-    
+
+    # curl -H "Authorization:Bearer YOUR_ACCESS_TOKEN" -k "https://services.cancerimagingarchive.net/nbia-api/services/getSeriesMetadata2" -d "list=1.3.6.1.4.1.14519.5.2.1.6834.5010.322628904903035357840500590726"
     # Helper functions
     def __str__(self):
         return self.value
