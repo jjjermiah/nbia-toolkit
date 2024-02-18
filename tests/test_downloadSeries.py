6874a3da725d3a4bb92ce1860bff8f7fb5ce3a53
Jermiah Joseph <jermiahjoseph98@gmail.com>
2024-02-18 15:40:51 -0500

    fix: ci verbose output

--- tests/test_downloadSeries.py
+++ tests/test_downloadSeries.py
@@ -2,7 +2,6 @@ import pytest
 from src.nbiatoolkit import NBIAClient
 from tempfile import TemporaryDirectory
 import os
-import requests
 
 
 @pytest.fixture
@@ -33,28 +32,3 @@ def test_downloadSeries(nbia_client):
         assert len(modality_dir) == 1
         assert modality_dir[0].startswith("CT")
         assert os.path.isdir(os.path.join(tempdir, dir[0], modality_dir[0]))
-
-    # assert len(dir) == 1
-    # assert os.path.isdir(os.path.join(tempdir, dir[0]))
-
-    # modality_dir = os.listdir(os.path.join(tempdir, dir[0]))
-    # assert len(modality_dir) == 1
-    # assert modality_dir[0] == "CT"
-    # assert os.path.isdir(os.path.join(tempdir, dir[0], modality_dir[0]))
-
-    # series_dir = os.listdir(os.path.join(tempdir, dir[0], modality_dir[0]))
-    # assert len(series_dir) == 1
-    # # only last 5 digits of SeriesInstanceUID are used
-    # assert series_dir[0] == "{}-{}".format(
-    #     seriesList[0]["SeriesNumber"], seriesList[0]["SeriesInstanceUID"][-5:]
-    # )
-    # assert os.path.isdir(os.path.join(tempdir, dir[0], modality_dir[0], series_dir[0]))
-
-    # dicom_dir = os.listdir(
-    #     os.path.join(tempdir, dir[0], modality_dir[0], series_dir[0])
-    # )
-
-    # assert len(dicom_dir) == int(seriesList[0]["ImageCount"])
-    # for file in dicom_dir:
-    #     assert file.endswith(".dcm")
-    #     assert file[:-4].isdigit()
