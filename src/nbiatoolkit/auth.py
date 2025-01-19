46446f4b4bdbaf9af24cfbfc1d2d379e6561f32f
Jermiah <jermiahjoseph98@gmail.com>
2025-01-19 16:47:35 +0000

    refactor: update dependencies in pyproject.toml and pixi.toml, and add settings management in nbiatoolkit

--- src/nbiatoolkit/auth.py
+++ src/nbiatoolkit/auth.py
@@ -9,304 +9,304 @@ from nbiatoolkit.utils import NBIA_BASE_URLS
 
 
 def encrypt_credentials(key: bytes, username: str, password: str) -> Tuple[str, str]:
-	"""
-	Encrypts the given username and password using the provided key.
+    """
+    Encrypts the given username and password using the provided key.
 
-	Parameters
-	----------
-		key (bytes): The encryption key.
-		username (str): The username to be encrypted.
-		password (str): The password to be encrypted.
+    Parameters
+    ----------
+            key (bytes): The encryption key.
+            username (str): The username to be encrypted.
+            password (str): The password to be encrypted.
 
-	Returns
-	-------
-	    Tuple[str, str]: A tuple containing the encrypted username and password.
-	"""
-	cipher_suite = Fernet(key=key)
-	encrypted_password = cipher_suite.encrypt(password.encode()).decode()
-	encrypted_username = cipher_suite.encrypt(username.encode()).decode()
-	return encrypted_username, encrypted_password
+    Returns
+    -------
+        Tuple[str, str]: A tuple containing the encrypted username and password.
+    """
+    cipher_suite = Fernet(key=key)
+    encrypted_password = cipher_suite.encrypt(password.encode()).decode()
+    encrypted_username = cipher_suite.encrypt(username.encode()).decode()
+    return encrypted_username, encrypted_password
 
 
 def decrypt_credentials(
-	key: bytes, encrypted_username: str, encrypted_password: str
+    key: bytes, encrypted_username: str, encrypted_password: str
 ) -> tuple[str, str]:
-	"""
-	Decrypts the encrypted username and password using the provided key.
+    """
+    Decrypts the encrypted username and password using the provided key.
 
-	Parameters
-	----------
-	    key (bytes): The encryption key used to decrypt the credentials.
-	    encrypted_username (str): The encrypted username.
-	    encrypted_password (str): The encrypted password.
+    Parameters
+    ----------
+        key (bytes): The encryption key used to decrypt the credentials.
+        encrypted_username (str): The encrypted username.
+        encrypted_password (str): The encrypted password.
 
-	Returns
-	-------
-	    tuple[str, str]: A tuple containing the decrypted username and password.
-	"""
-	cipher_suite = Fernet(key=key)
-	decrypted_username = cipher_suite.decrypt(encrypted_username.encode()).decode()
-	decrypted_password = cipher_suite.decrypt(encrypted_password.encode()).decode()
-	# return the decrypted client_id and username
-	return decrypted_username, decrypted_password
+    Returns
+    -------
+        tuple[str, str]: A tuple containing the decrypted username and password.
+    """
+    cipher_suite = Fernet(key=key)
+    decrypted_username = cipher_suite.decrypt(encrypted_username.encode()).decode()
+    decrypted_password = cipher_suite.decrypt(encrypted_password.encode()).decode()
+    # return the decrypted client_id and username
+    return decrypted_username, decrypted_password
 
 
 class OAuth2:
-	"""
-	OAuth2 class for handling authentication and access token retrieval.
+    """
+    OAuth2 class for handling authentication and access token retrieval.
 
-	This class provides methods to authenticate with the NBIA API using OAuth2
-	and retrieve the access token required for accessing the API.
+    This class provides methods to authenticate with the NBIA API using OAuth2
+    and retrieve the access token required for accessing the API.
 
-	Defaults to using the NBIA Guest for accessing public collections.
-	If you have a username and password which has been granted access
-	to the collections tagged with "limited access" you can use those
-	credentials to access those collections.
+    Defaults to using the NBIA Guest for accessing public collections.
+    If you have a username and password which has been granted access
+    to the collections tagged with "limited access" you can use those
+    credentials to access those collections.
 
 
-	Attributes
-	----------
-	client_id : str
-	    The client ID for authentication.
-	username : str
-	    The username for authentication.
-	password : str
-	    The password for authentication.
-	access_token : str or None
-	    The access token retrieved from the API.
-	api_headers : dict or None
-	    The authentication headers containing the access token.
-	expiry_time : str or None
-	    The expiry time of the access token.
-	refresh_token : str or None
-	    The refresh token for obtaining a new access token.
-	refresh_expiry : int or None
-	    The expiry time of the refresh token.
-	scope : str or None
-	    The scope of the access token.
+    Attributes
+    ----------
+    client_id : str
+        The client ID for authentication.
+    username : str
+        The username for authentication.
+    password : str
+        The password for authentication.
+    access_token : str or None
+        The access token retrieved from the API.
+    api_headers : dict or None
+        The authentication headers containing the access token.
+    expiry_time : str or None
+        The expiry time of the access token.
+    refresh_token : str or None
+        The refresh token for obtaining a new access token.
+    refresh_expiry : int or None
+        The expiry time of the refresh token.
+    scope : str or None
+        The scope of the access token.
 
-	Methods
-	-------
-	getToken()
-	    Authenticates with the API. Returns API headers containing the
-	    access token.
+    Methods
+    -------
+    getToken()
+        Authenticates with the API. Returns API headers containing the
+        access token.
 
-	Example Usage
-	-------------
-	>>> from nbiatoolkit.auth import (
-	...     OAuth2,
-	... )
+    Example Usage
+    -------------
+    >>> from nbiatoolkit.auth import (
+    ...     OAuth2,
+    ... )
 
-	To use the NBIA Guest account:
+    To use the NBIA Guest account:
 
-	>>> oauth = OAuth2()
+    >>> oauth = OAuth2()
 
-	To use a custom account:
+    To use a custom account:
 
-	>>> oauth = OAuth2(
-	...     username='my_username',
-	...     password='my_password',
-	... )
+    >>> oauth = OAuth2(
+    ...     username='my_username',
+    ...     password='my_password',
+    ... )
 
-	Notes
-	-----
-	This class is mainly for developers looking to add functionality
-	to the nbiatoolkit package. If you are a user looking to access the NBIA
-	API, you can use the `NBIAClient` class without knowledge of this class.
+    Notes
+    -----
+    This class is mainly for developers looking to add functionality
+    to the nbiatoolkit package. If you are a user looking to access the NBIA
+    API, you can use the `NBIAClient` class without knowledge of this class.
 
-	As there are many packages for handling OAuth2 authentication, this class
-	was for myself to learn how OAuth2 works and to provide a simple way to
-	authenticate with the NBIA API. If you have any suggestions for improving
-	this class, please open an issue on the GitHub repository.
-	"""
+    As there are many packages for handling OAuth2 authentication, this class
+    was for myself to learn how OAuth2 works and to provide a simple way to
+    authenticate with the NBIA API. If you have any suggestions for improving
+    this class, please open an issue on the GitHub repository.
+    """
 
-	def __init__(
-		self,
-		username: str = 'nbia_guest',
-		password: str = '',
-		client_id: str = 'NBIA',
-		base_url: str | NBIA_BASE_URLS = NBIA_BASE_URLS.NBIA,
-	) -> None:
-		"""
-		Initialize the OAuth2 class.
+    def __init__(
+        self,
+        username: str = "nbia_guest",
+        password: str = "",
+        client_id: str = "NBIA",
+        base_url: str | NBIA_BASE_URLS = NBIA_BASE_URLS.NBIA,
+    ) -> None:
+        """
+        Initialize the OAuth2 class.
 
-		Parameters
-		----------
-		username : str, optional
-		    The username for authentication. Default is "nbia_guest".
-		password : str, optional
-		    The password for authentication. Default is an empty string.
-		client_id : str, optional
-		    The client ID for authentication. Default is "NBIA".
-		base_url : str or NBIA_BASE_URLS, optional. Default is NBIA_BASE_URLS.NBIA
+        Parameters
+        ----------
+        username : str, optional
+            The username for authentication. Default is "nbia_guest".
+        password : str, optional
+            The password for authentication. Default is an empty string.
+        client_id : str, optional
+            The client ID for authentication. Default is "NBIA".
+        base_url : str or NBIA_BASE_URLS, optional. Default is NBIA_BASE_URLS.NBIA
 
-		"""
-		self.client_id = client_id
+        """
+        self.client_id = client_id
 
-		self._fernet_key: bytes = Fernet.generate_key()
-		self.username: str
-		self.password: str
+        self._fernet_key: bytes = Fernet.generate_key()
+        self.username: str
+        self.password: str
 
-		self.username, self.password = encrypt_credentials(
-			key=self._fernet_key, username=username, password=password
-		)
+        self.username, self.password = encrypt_credentials(
+            key=self._fernet_key, username=username, password=password
+        )
 
-		if isinstance(base_url, NBIA_BASE_URLS):
-			self.base_url = base_url.value
-		else:
-			self.base_url = base_url
-		logger.debug('OAuth2 client initialized with username %s', self.username)
-		logger.debug('base_url: %s', self.base_url)
+        if isinstance(base_url, NBIA_BASE_URLS):
+            self.base_url = base_url.value
+        else:
+            self.base_url = base_url
+        logger.debug("OAuth2 client initialized with username %s", self.username)
+        logger.debug("base_url: %s", self.base_url)
 
-		self._access_token = None
-		self.expiry_time: int | None = None
-		self.refresh_expiry = None
-		self.refresh_token = ''  # Fix: Assign an empty string instead of None
-		self.scope = None
+        self._access_token = None
+        self.expiry_time: int | None = None
+        self.refresh_expiry = None
+        self.refresh_token = ""  # Fix: Assign an empty string instead of None
+        self.scope = None
 
-	@property
-	def fernet_key(self) -> bytes:  # noqa
-		return self._fernet_key
+    @property
+    def fernet_key(self) -> bytes:  # noqa
+        return self._fernet_key
 
-	def is_logged_out(self) -> bool:  # noqa
-		return (
-			self._access_token == None and self.username == '' and self.password == ''
-		)
+    def is_logged_out(self) -> bool:  # noqa
+        return (
+            self._access_token is None and self.username == "" and self.password == ""
+        )
 
-	@property
-	def access_token(self) -> str | None:  # noqa
-		if self.is_logged_out():
-			return None
+    @property
+    def access_token(self) -> str | None:  # noqa
+        if self.is_logged_out():
+            return None
 
-		# Check if access token is not set or it's expired
-		if self.is_token_expired() or self._access_token is None:
-			self.refresh_token_or_request_new()
+        # Check if access token is not set or it's expired
+        if self.is_token_expired() or self._access_token is None:
+            self.refresh_token_or_request_new()
 
-		return self._access_token
+        return self._access_token
 
-	def is_token_expired(self) -> bool:  # noqa
-		# Check if the token expiration time is set and if it's expired
-		return self.expiry_time is not None and time.time() > self.expiry_time
+    def is_token_expired(self) -> bool:  # noqa
+        # Check if the token expiration time is set and if it's expired
+        return self.expiry_time is not None and time.time() > self.expiry_time
 
-	def refresh_token_or_request_new(self) -> None:  # noqa
-		if self.refresh_token != '':
-			self._refresh_access_token()
-		else:
-			self.request_new_access_token()
+    def refresh_token_or_request_new(self) -> None:  # noqa
+        if self.refresh_token != "":
+            self._refresh_access_token()
+        else:
+            self.request_new_access_token()
 
-	def _refresh_access_token(self) -> None:
-		assert self.refresh_token != '', 'Refresh token is not set'
+    def _refresh_access_token(self) -> None:
+        assert self.refresh_token != "", "Refresh token is not set"
 
-		# Prepare the request data
-		data: dict[str, str] = {
-			'refresh_token': self.refresh_token,
-			'client_id': self.client_id,
-			'grant_type': 'refresh_token',
-		}
+        # Prepare the request data
+        data: dict[str, str] = {
+            "refresh_token": self.refresh_token,
+            "client_id": self.client_id,
+            "grant_type": "refresh_token",
+        }
 
-		token_url: str = self.base_url + 'oauth/token'
+        token_url: str = self.base_url + "oauth/token"
 
-		response = requests.post(token_url, data=data)
+        response = requests.post(token_url, data=data)
 
-		response.raise_for_status()
-		# requests.exceptions.HTTPError:
+        response.raise_for_status()
+        # requests.exceptions.HTTPError:
 
-		token_data = response.json()
-		self.set_token_data(token_data)
+        token_data = response.json()
+        self.set_token_data(token_data)
 
-	def request_new_access_token(self) -> None:  # noqa
-		data: dict[str, str] = {
-			'username': decrypt_credentials(
-				key=self.fernet_key,
-				encrypted_username=self.username,
-				encrypted_password=self.password,
-			)[0],
-			'password': decrypt_credentials(
-				key=self.fernet_key,
-				encrypted_username=self.username,
-				encrypted_password=self.password,
-			)[1],
-			'client_id': self.client_id,
-			'grant_type': 'password',
-		}
+    def request_new_access_token(self) -> None:  # noqa
+        data: dict[str, str] = {
+            "username": decrypt_credentials(
+                key=self.fernet_key,
+                encrypted_username=self.username,
+                encrypted_password=self.password,
+            )[0],
+            "password": decrypt_credentials(
+                key=self.fernet_key,
+                encrypted_username=self.username,
+                encrypted_password=self.password,
+            )[1],
+            "client_id": self.client_id,
+            "grant_type": "password",
+        }
 
-		token_url: str = self.base_url + 'oauth/token'
+        token_url: str = self.base_url + "oauth/token"
 
-		response: requests.models.Response
-		response = requests.post(token_url, data=data)
+        response: requests.models.Response
+        response = requests.post(token_url, data=data)
 
-		response = requests.post(token_url, data=data)
-		response.raise_for_status()
-		# requests.exceptions.HTTPError:
+        response = requests.post(token_url, data=data)
+        response.raise_for_status()
+        # requests.exceptions.HTTPError:
 
-		token_data = response.json()
-		self.set_token_data(token_data)
+        token_data = response.json()
+        self.set_token_data(token_data)
 
-	def set_token_data(self, token_data: dict) -> None:
-		self._access_token = token_data['access_token']
-		self.expiry_time = int(time.time()) + int(token_data.get('expires_in') or 0)
-		self.refresh_token = token_data['refresh_token']
-		self.refresh_expiry = token_data.get('refresh_expires_in')
-		self.scope = token_data.get('scope')
+    def set_token_data(self, token_data: dict) -> None:
+        self._access_token = token_data["access_token"]
+        self.expiry_time = int(time.time()) + int(token_data.get("expires_in") or 0)
+        self.refresh_token = token_data["refresh_token"]
+        self.refresh_expiry = token_data.get("refresh_expires_in")
+        self.scope = token_data.get("scope")
 
-	@property
-	def api_headers(self) -> dict[str, str]:  # noqa
-		return {
-			'Authorization': f'Bearer {self.access_token}',
-			'Content-Type': 'application/json',
-		}
+    @property
+    def api_headers(self) -> dict[str, str]:  # noqa
+        return {
+            "Authorization": f"Bearer {self.access_token}",
+            "Content-Type": "application/json",
+        }
 
-	@property
-	def token_expiration_time(self):  # noqa
-		return self.expiry_time
+    @property
+    def token_expiration_time(self):  # noqa
+        return self.expiry_time
 
-	@property
-	def refresh_expiration_time(self):  # noqa
-		return self.refresh_expiry
+    @property
+    def refresh_expiration_time(self):  # noqa
+        return self.refresh_expiry
 
-	@property
-	def token_scope(self):  # noqa
-		return self.scope
+    @property
+    def token_scope(self):  # noqa
+        return self.scope
 
-	def __repr__(self) -> str:  # noqa
-		if self.username:
-			return f'OAuth2(username={self.username}, client_id={self.client_id})'
-		else:
-			return ''
+    def __repr__(self) -> str:  # noqa
+        if self.username:
+            return f"OAuth2(username={self.username}, client_id={self.client_id})"
+        else:
+            return ""
 
-	def __str__(self):  # noqa
-		if self.username:
-			return f'OAuth2(username={self.username}, client_id={self.client_id})'
-		else:
-			return ''
+    def __str__(self):  # noqa
+        if self.username:
+            return f"OAuth2(username={self.username}, client_id={self.client_id})"
+        else:
+            return ""
 
-	def logout(self) -> None:
-		"""Log out the user and revokes the access token.
+    def logout(self) -> None:
+        """Log out the user and revokes the access token.
 
-		This method sends a request to the NBIA API to revoke the access token
-		and logs out the user.
+        This method sends a request to the NBIA API to revoke the access token
+        and logs out the user.
 
-		Notes
-		-----
-		This method is not yet implemented in the NBIA API.
-		"""
-		if not self.access_token:
-			return None
+        Notes
+        -----
+        This method is not yet implemented in the NBIA API.
+        """
+        if not self.access_token:
+            return None
 
-		query_url = NBIA_BASE_URLS.LOGOUT_URL.value
-		response = requests.get(query_url, headers=self.api_headers)
-		try:
-			response.raise_for_status()
-		except requests.exceptions.HTTPError:
-			pass  # WAIT UNTIL TCIA IMPLEMENTS LOGOUT FUNCTIONALITY
-		finally:
-			# set the entire object to None
-			self.username = ''
-			self.password = ''
-			self.client_id = ''
-			self.base_url = ''
-			self._access_token = None
-			self.expiry_time = None
-			self.refresh_expiry = None
-			self.refresh_token = ''
-			self.scope = None
+        query_url = NBIA_BASE_URLS.LOGOUT_URL.value
+        response = requests.get(query_url, headers=self.api_headers)
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            pass  # WAIT UNTIL TCIA IMPLEMENTS LOGOUT FUNCTIONALITY
+        finally:
+            # set the entire object to None
+            self.username = ""
+            self.password = ""
+            self.client_id = ""
+            self.base_url = ""
+            self._access_token = None
+            self.expiry_time = None
+            self.refresh_expiry = None
+            self.refresh_token = ""
+            self.scope = None
---
+++ src/nbiatoolkit/settings.py
@@ -0,0 +1,67 @@
+from enum import StrEnum, auto
+from pathlib import Path
+from typing import Tuple, Type
+
+from pydantic import BaseModel, model_validator
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+    TomlConfigSettingsSource,
+)
+
+
+class Login(BaseModel):
+    nbia_username: str = "nbia_guest"
+    nbia_password: str = ""
+
+    def write_toml(self, path: Path) -> None:
+        import toml  # noqa
+
+        data = {
+            "login": {
+                "nbia_username": self.nbia_username,
+                "nbia_password": self.nbia_password,
+            }
+        }
+        with path.open("w") as toml_file:
+            toml.dump(data, toml_file)
+
+
+class Settings(BaseSettings):
+    # project_name: str | None = None
+    login: Login = Login()
+
+    model_config = SettingsConfigDict(
+        env_nested_delimiter=".",
+        env_file=".env",
+        env_file_encoding="utf-8",
+        yaml_file="nbia.yaml",
+        toml_file=Path("~/.config", "nbiatoolkit") / "settings.toml",
+        # allow for other fields to be present in the config file
+        # this allows for the config file to be used for other purposes
+        # but also for users to define anything else they might want
+        extra="ignore",
+    )
+
+    @property
+    def dataset_names(self) -> list[str]:
+        return self.datasets.keys()
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        return (
+            # env_settings,
+            # dotenv_settings,
+            # file_secret_settings,
+            init_settings,
+            TomlConfigSettingsSource(settings_cls),
+            # YamlConfigSettingsSource(settings_cls),
+        )
